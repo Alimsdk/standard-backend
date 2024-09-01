947a7d9e8c88364074233e1c9404610200375c8e
Alimsdk <alimsiddique04@gmail.com>
2024-09-02 04:21:04 +0600

    database connected successfully

---
+++ src/db/index.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    console.log(process.env.MONGODB_URI, DB_NAME);
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n Mongodb connected !! DB HOST : ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB CONNECTION ERROR :", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
